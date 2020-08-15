8f676560708731227cf025f3c81747bbc3078fca
shama2369 <shama2369@gmail.com>
2020-08-16 00:26:36 +0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploadssham"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://nsogac5c50.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_6esarEwoA",
+    APP_CLIENT_ID: "482mv58b0qji6vsave542msovj",
+    IDENTITY_POOL_ID: "us-east-1:62f20b3f-7461-48de-b130-28e07ef59efe"
+  }
+};
