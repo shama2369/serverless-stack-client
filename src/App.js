f46da25a41d039a58e36cb9ec5a5bd729d2e88b7
shama2369 <shama2369@gmail.com>
2020-08-15 22:59:57 +0400

    last changed1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar } from "react-bootstrap";
 import "./App.css";
-
+import Routes from "./Routes";
 function App() {
   return (
     <div className="App container">
